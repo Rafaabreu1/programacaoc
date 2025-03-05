fee61252cefdb9e327b0f68ecb68d1525814e629
Rafael De Abreu <abreurafa1@hotmail.com>
2025-03-05 04:03:26 +0000

    Super trunfo nível Mestre

--- cadastro_cartas_super_trunfo.c
+++ cadastro_cartas_super_trunfo.c
@@ -9,7 +9,8 @@ int main() {
     float area;
     float pib;
     int numero_pontos_turisticos;
-     
+    float super_poder;
+
      //variáveis carta 2
     char codigo_da_cidade2 [50];
     char estado2[50];
@@ -18,6 +19,7 @@ int main() {
     float area2;
     float pib2;
     int numero_pontos_turisticos2;
+    float super_poder2;
 
     //variáveis de comparação
     unsigned long int resultado_populacao;
@@ -26,6 +28,7 @@ int main() {
     int resultado_numero_pontos_turisticos;
     float resultado_densidade_populacional;
     float resultado_pib_per_capita;
+    float resultado_super_poder;
     
       //Entrada carta 1
     printf ("Digite os dados da carta 1:\n");
@@ -53,6 +56,7 @@ int main() {
 
     float densidade_populacional = populacao /  area;
     float pib_per_capita = pib / populacao;
+    float super_poder = populacao + area + pib + numero_pontos_turisticos + pib_per_capita + (1/densidade_populacional);
 
     getchar();
 
@@ -83,10 +87,17 @@ int main() {
 
     float densidade_populacional2 = populacao2 /  area2;
     float pib_per_capita2 = pib2 / populacao2;
+    float super_poder2 = populacao2 + area2 + pib2 + numero_pontos_turisticos2 + pib_per_capita2 + (1/densidade_populacional2);
 
     //Comparações entre as cartas 
 
     resultado_populacao = populacao > populacao2;
+    resultado_area = area > area2;
+    resultado_pib = pib > pib2;
+    resultado_numero_pontos_turisticos = numero_pontos_turisticos > numero_pontos_turisticos2;
+    resultado_pib_per_capita = pib_per_capita > pib_per_capita2;
+    resultado_densidade_populacional = densidade_populacional > densidade_populacional2;
+    resultado_super_poder = super_poder > super_poder2;
 
     //saída carta 1
 
@@ -101,8 +112,9 @@ int main() {
     printf ("Número de pontos turísticos: %d\n", numero_pontos_turisticos);
     printf ("Densidade populacional: %.2f hab/km²\n", densidade_populacional);
     printf ("PIB per capita: %.2f\n", pib_per_capita);
+    printf ("super poder: %.2f\n", super_poder);
 
-    //saída carta 1
+    //saída carta 2
     
     printf ("\n--- Carta 2 ---\n");
     printf ("\n--- Dados da Cidade ---\n");
@@ -115,18 +127,18 @@ int main() {
     printf ("Número de pontos turísticos: %d\n",numero_pontos_turisticos2);
     printf ("Densidade populacional: %.2f hab/km²\n",densidade_populacional2);
     printf ("PIB per capita: %.2f\n",pib_per_capita2);
+    printf ("super poder: %.2f\n", super_poder2);
 
     // Saída das comparações 
     
     printf ("--comparação entre as cartas--\n");
     printf ("População da carta 1 é maior? = %d\n",resultado_populacao );
-    printf ("Área da carta 1 é maior? = %.d\n",resultado_area);
-    printf ("PIB da carta 1 é maior? = %.d\n",resultado_pib);
-    printf ("Número de pontos turísticos da carta 1 é maior? = %.d\n",resultado_numero_pontos_turisticos);
-    printf ("Densidade populacional da carta 1 é maior? = %.d\n",resultado_densidade_populacional);
-    printf ("Pib per capita da carta 1 é maior? = %.d\n",resultado_pib_per_capitadsf
-    dsf
-);
+    printf ("Área da carta 1 é maior? = %d\n",resultado_area);
+    printf ("PIB da carta 1 é maior? = %d\n",resultado_pib);
+    printf ("Número de pontos turísticos da carta 1 é maior? = %d\n",resultado_numero_pontos_turisticos);
+    printf ("Densidade populacional da carta 1 é maior? = %d\n",resultado_densidade_populacional);
+    printf ("Pib per capita da carta 1 é maior? = %d\n",resultado_pib_per_capita);
+    printf ("Super Poder da carta 1 é maior? = %d\n",resultado_super_poder);
 
     return 0;
 }
